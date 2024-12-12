67eff1133d08d97ee4679911b5a8d7896a979190
iivyn2 <idnguyen101@student.hbuhsd.edu>
2024-12-12 21:49:42 +0000

    almost finished exercise 1

--- classwork.cpp
+++ classwork.cpp
@@ -3,13 +3,28 @@
 
 int main()
 {
-  int age = 10;
-double price = 19.99;
-  bool isRaining = false; 
+ 
+int x;
+int y;
+int add_xy;
+int sub_xy;
+int x = 25; 
+int y = 3; 
+std::cout << add_xy << std::endl
+<< add_xy << std::endl
+<< sub_xy<< std:: endl 
 
-  std::cout << age << "\n" << price << std::endl << isRaining;
+bool isSunny = true;
+bool hasUmbrella = false;
+std::cout<<(isSunny && hasUmbrella) << std::endl;
 
-  int grade_level = 11;
-  std:: cout << "I am a " << grade_level << "th grader." << std::endl;
+bool sun_or_umb = isSunny || hasUmbrella;
+std::cout<<sun_or_umb << "\n";
+
+//Exercise 3
+int n = 7;
+double m = 4.0;
+bool hasLicense = false;
+
   return 0;
 }
