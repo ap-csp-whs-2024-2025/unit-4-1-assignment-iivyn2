28b6f5dc324dad916741e69ae38a1050761957e6
iivyn2 <idnguyen101@student.hbuhsd.edu>
2024-12-13 21:49:25 +0000

    did exercise 1 and 2

--- classwork.cpp
+++ classwork.cpp
@@ -1,30 +1,54 @@
 #include <iostream>
 #include <string>
 
-int main()
+int main( )
 {
- 
-int x;
-int y;
-int add_xy;
-int sub_xy;
-int x = 25; 
-int y = 3; 
-std::cout << add_xy << std::endl
-<< add_xy << std::endl
-<< sub_xy<< std:: endl 
+ //exercise1
 
-bool isSunny = true;
-bool hasUmbrella = false;
-std::cout<<(isSunny && hasUmbrella) << std::endl;
+  int x;
+  int y;
+  int add_xy;
+  int sub_xy;
+  int div_xy;
+  int mult_xy;
+  int mod_xy;
+  x = 25; 
+  y = 3; 
 
-bool sun_or_umb = isSunny || hasUmbrella;
-std::cout<<sun_or_umb << "\n";
 
-//Exercise 3
-int n = 7;
+  add_xy = x + y; 
+  sub_xy = x - y;
+  mult_xy = x * y; 
+  div_xy = x/y;
+  mod_xy = x % y;
+  std::cout << "x + y= " << add_xy << std::endl;
+  std::cout << "x - y= " << sub_xy << std:: endl; 
+  std::cout << "x * y= " << mult_xy << std:: endl;
+  std::cout << "x / y= " << div_xy << std:: endl;
+  std::cout << "x mod y= " << mod_xy << std:: endl;
+
+  int z = 10; 
+  bool xGreaterz = x > z;
+  bool ylessThanx = y < x;
+  std::cout << xGreaterz << std::endl;
+  std::cout << ylessThanx << std::endl;
+
+//exercise2
+bool hasUmbrella = true;
+bool isSunny = false; 
+
+std::cout << "Is it both sunny and do you have an umbrella? " << (isSunny && hasUmbrella) <<  std::endl;
+std::cout << "Is it either sunny or do you have an umbrella? " << (isSunny || hasUmbrella) << std::endl;
+std::cout << "Is it not sunny? " << (!isSunny) << std::endl;
+std::cout << "Is it not sunny and you don't have an umbrella? " << (!isSunny && !hasUmbrella) << std::endl; 
+
+//exercise3
+int n = 7; 
 double m = 4.0;
-bool hasLicense = false;
+bool hasLicense = false; 
 
+std::cout << "Is n greater than m" << (n )
   return 0;
+
+
 }
